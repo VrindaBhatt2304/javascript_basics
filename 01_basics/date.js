ac275e0b9e8b08b32023e9980d388d25e2d9cfad
VrindaBhatt2304 <vrindabhatt02@gmail.com>
2025-06-16 12:11:30 +0000

    initial commit

---
+++ 01_basics/date.js
@@ -0,0 +1,7 @@
+let mydate=new Date();
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(typeof mydate);
