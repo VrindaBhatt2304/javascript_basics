c75bebf9b95d8af998ff05643dec455088a0f875
VrindaBhatt2304 <vrindabhatt02@gmail.com>
2025-06-16 07:56:39 +0000

    done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+//alert("hello") we are using nnodejs not web browser
+
+console.log(3+3);
+
+let name="vrinda"
+let age=23
+let isLoggedIn=false
+
+//int bigint
+//string=""
+//boolean true/false
+//null stand alone value
+//undefined variable is declared but not assigned
+//symbol
+
+//object
+
+console.log(typeof age);
+console.log(typeof null)
+console.log(typeof name)
+
